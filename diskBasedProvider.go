a45a08d1eb20a56a7851a1671ee0e9622ca39f2b
Adam Hintz <adamh.zero@gmail.com>
2020-03-29 16:10:23 -0700

    Actually render pages and stuff

--- diskBasedProvider.go
+++ diskBasedProvider.go
@@ -2,6 +2,8 @@ package blarg
 
 import (
 	"fmt"
+	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -9,18 +11,28 @@ import (
 	"strings"
 
 	"github.com/adam000/blarg/rule"
+	"github.com/adam000/goutils/page"
+	"github.com/microcosm-cc/bluemonday"
+	"github.com/russross/blackfriday"
 )
+
+// TODO at some point this type should change -- some statuses are more than just a code
+type ErrorHandler func(http.ResponseWriter, *http.Request, int)
 
 type DiskBasedProvider struct {
 	baseUrl       string
 	baseDirectory string
 	rules         []rule.Rule
+	templates     *template.Template
+	errorHandler  ErrorHandler
 }
 
-func NewDiskBasedProvider(baseDirectory string, baseUrl string) DiskBasedProvider {
+func NewDiskBasedProvider(baseDirectory string, baseUrl string, templates *template.Template, errorHandler ErrorHandler) DiskBasedProvider {
 	return DiskBasedProvider{
 		baseUrl:       baseUrl,
 		baseDirectory: baseDirectory,
+		templates:     templates,
+		errorHandler:  errorHandler,
 	}
 }
 
@@ -34,24 +46,61 @@ func (p DiskBasedProvider) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	baseLower := strings.ToLower(p.baseUrl)
 	if !strings.HasPrefix(requestLower, baseLower) {
 		fmt.Fprintf(w, "Fatal error: %s needs to start with %s or else I can't figure out what to do with it", requestLower, baseLower)
+		p.errorHandler(w, r, http.StatusInternalServerError)
 		return
 	}
 
-	// TODO: report if file exists
 	pathRelative := r.URL.String()[len(baseLower):]
 
 	pathAbsolute, err := filepath.Abs(filepath.Join(filepath.Dir(os.Args[0]), p.baseDirectory, pathRelative))
 	if err != nil {
 		log.Printf("Error trying to get absolute path of %s: %v", pathRelative, err)
+		p.errorHandler(w, r, http.StatusInternalServerError)
 		return
 	}
 
 	log.Printf("Looking for file at '%s' (absolute path: '%s')", pathRelative, pathAbsolute)
-	if exists, _ := Exists(pathAbsolute); exists {
-		log.Printf("File exists!")
+	if exists, _ := Exists(pathAbsolute); !exists {
+		mdPath := pathAbsolute + ".md"
+		if mdExists, _ := Exists(mdPath); mdExists {
+			log.Printf("Serving up %s", mdPath)
+			p.serveMd(w, r, mdPath)
+			return
+		}
+
+		log.Printf("File does not exist!")
+		p.errorHandler(w, r, http.StatusNotFound)
+		return
 	}
 
-	fmt.Fprintf(w, "Done!")
+	if isDir, _ := IsDir(pathAbsolute); isDir {
+		fmt.Fprintf(w, "TODO implement directory support")
+		return
+	}
+
+	http.ServeFile(w, r, pathAbsolute)
+}
+
+func (p DiskBasedProvider) serveMd(w http.ResponseWriter, r *http.Request, mdPath string) {
+	markdown, err := ioutil.ReadFile(mdPath)
+	if err != nil {
+		log.Printf("Error reading MD file: %s", markdown)
+		p.errorHandler(w, r, http.StatusInternalServerError)
+	}
+
+	htmlResult := template.HTML(bluemonday.UGCPolicy().SanitizeBytes(blackfriday.MarkdownCommon(markdown)))
+
+	// TODO some of this is specific to my website. Abstract it out.
+	var page = page.NewPage()
+	page.SetTitle("Blarg")
+	page.AddCSSFiles(
+		"/static/css/base.css",
+		"/static/css/header.css",
+		"/static/css/blarg-file.css",
+	)
+	page.AddVar("Content", htmlResult)
+	page.AddVar("FileName", strings.TrimSuffix(filepath.Base(mdPath), filepath.Ext(mdPath)))
+	p.templates.ExecuteTemplate(w, "page_blarg_file.html", page)
 }
 
 func Exists(path string) (bool, error) {
@@ -63,4 +112,12 @@ func Exists(path string) (bool, error) {
 		return false, nil
 	}
 	return true, err
+}
+
+func IsDir(path string) (bool, error) {
+	stat, err := os.Stat(path)
+	if err != nil {
+		return false, err
+	}
+	return stat.IsDir(), nil
 }
